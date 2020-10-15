d88147940ce41436d71e5d4ea9acd021edc2284c
Colin McLellan <expiredserver@gmail.com>
2020-10-15 08:16:14 -0400

    finished seed data for booksdb fullly working

--- server/db/seeds/01_books.sql
+++ server/db/seeds/01_books.sql
@@ -2,8 +2,16 @@ INSERT INTO
   books
   (title, author, quantity)
 VALUES
-  ('Python Crash Course', 'Eric Matthes', '5'),
+  ('Python Crash Course', 'Eric Matthes', 5),
 
-  ('Head-First Python', 'Paul Barry', '2'),
+  ('Head-First Python', 'Paul Barry', 2),
 
-  ('Invent Your Own Computer Games with Python', 'Al Sweigart', '1')
+  ('Invent Your Own Computer Games with Python', 'Al Sweigart', 1),
+
+('Think Python: How to Think Like a Computer Scientist', 'Allen B. Downey', 15),
+ ('Effective Computation in Physics: Field Guide to Research with Python', 'Anthony Scopatz, Kathryn D. Huff', 7),
+  ('Learn Python 3 the Hard Way', 'Zed A. Shaw', 0),
+  ('The Vue Handbook', 'Flavio Copes', 0),
+  ('Vue.js: Up and Running', 'Callum Macrae', 1),
+  ('Fullstack Vue', 'Hassan Djirdeh', 3),
+  ('Vue.js 2 and Bootstrap 4 Web Development', 'Olga Filipova', 21)
