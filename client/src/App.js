d920de3ac64be50dea5cf5c4a48cfae8ba1b36ae
Colin McLellan <expiredserver@gmail.com>
2020-10-15 09:04:59 -0400

    fixed cors policy issue and created first route able to return all books from db

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
 
 function App() {
+  useEffect(() => {
+
+    
+      axios
+        .get(`http://localhost:3001/api/books`)
+        .then((all) => {
+          console.log(all.data);
+        })
+    
+
+
+    
+
+      }, [])
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +34,7 @@ function App() {
         >
           Learn React
         </a>
+        
       </header>
     </div>
   );
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
+const db = require("../db");
+
+const getBooks = () => {
+  const query = {
+    text: `SELECT * FROM books`,
+  };
+
+  return db
+    .query(query)
+    .then((result) => result.rows)
+    .catch((err) => err);
+};
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+
+router.get("/api/books", (req, res) => {
+  getBooks(req.params.id)
+    .then((data) => {
+      console.log("appts: ", data.data);
+      return res.json(data);
+    })
+    .catch((err) => res.json({ err }));
 });
 
 module.exports = router;
