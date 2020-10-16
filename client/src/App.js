26cfd216155da5c908c6abeeafc6a4b84d2994ac
Colin McLellan <expiredserver@gmail.com>
2020-10-15 22:05:00 -0400

    built out status logic need to confirm meaning of status in requirements

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,14 @@ import React, { useState } from "react";
 import "./App.css";
 import axios from "axios";
 import BooksList from './components/BooksList'
+import { Header } from "semantic-ui-react";
 
 function App() {
   
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Welcome To Your Digital Library</h1>
+        
         <BooksList/>
       </header>
     </div>
